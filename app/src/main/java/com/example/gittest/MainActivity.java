2a343ad596344fd319a75e5a8d03e374138ac0d0
Sheldon <100086149@students.swinburne.edu.my>
2019-09-03 09:44:44 +0800

    Test 5

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
+        super.finalize();
     }
 }
