13309481f52d57d29a776a9fdd0bfe10836a9afd
Sheldon <100086149@students.swinburne.edu.my>
2019-09-03 09:32:00 +0800

    Revert "Test 2"
    This reverts commit badad27b

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -12,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+        print(Hello);
+    }
 }
