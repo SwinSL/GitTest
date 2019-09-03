6851835eb8674fc4a3deab7746ca268e4123b3ca
Sheldon <100086149@students.swinburne.edu.my>
2019-09-03 09:26:07 +0800

    Test 1

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+        print(Hello);
     }
 }
